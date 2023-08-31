309d3bdf692f30836a01bc67be978eea90f3acbb
Johnson Wang <drgarbage@gmail.com>
2023-08-31 11:40:15 +0800

    change image

--- console/pages/index.js
+++ console/pages/index.js
@@ -1,20 +1,23 @@
-import { Button, Card, CardActions, CardContent, CardHeader, Container, Grid, Typography } from "@mui/material"
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Grid, Typography } from "@mui/material"
 
 export default () => {
   return (
-    <Container sx={{flex:1, display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+    <Container sx={{flex:1, height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
       <Grid container justifyContent="center">
         <Grid item xs={12} md={6} lg={4} flex={1}>
           <Card>
-            <CardHeader title="Biz Bot" />
-            <CardContent>
-              <Typography>
-                This is a line application for small bisiness.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button fullWidth variant="contained">Config</Button>
-            </CardActions>
+            <CardMedia 
+              component="img"
+              height={320}
+              image="/images/banner.png" 
+              />
+            <CardHeader 
+              avatar={
+                <Avatar src="/images/girl.png" />
+              }
+              title="會計小幫手" 
+              subheader="歡迎加入會計小幫手LINE官方帳號"
+              />
           </Card>
         </Grid>
       </Grid>
